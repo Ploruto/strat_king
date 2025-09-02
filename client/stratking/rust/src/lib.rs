ff5fc79873fb9f66a43e44adf27168a3f65f35d3
Philipp Haack <PhilippHaackCode@gmail.com>
2025-09-02 19:32:47 +0200

    init godot client

---
+++ client/stratking/rust/src/lib.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+use godot::prelude::*;
+use godot_bevy::prelude::*;
+
+#[bevy_app]
+fn build_app(app: &mut App) {
+    // GodotDefaultPlugins provides all standard godot-bevy functionality
+    app.add_plugins(GodotDefaultPlugins);
+
+    // Add your systems here
+    app.add_systems(Update, hello_world_system);
+}
+
+fn hello_world_system(mut timer: Local<f32>, time: Res<Time>) {
+    // This runs every frame in Bevy's Update schedule
+    *timer += time.delta_secs();
+    if *timer > 1.0 {
+        *timer = 0.0;
+        godot_print!("Hello from Bevy ECS!");
+    }
+}
